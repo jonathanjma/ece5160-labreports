58fabf59903fb8ede51d633b719c84ac9988dc92
jonathanjma <jonathanjma03@gmail.com>
2026-04-21 01:24:35 +0000

    Deploying to gh-pages from @ jonathanjma/ece5160-labreports@31a262d84cfb616dbe2ec6cf17561c1b0f048d0e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,6 +21,11 @@ ninja.data = [{
           description: "Artemis and Bluetooth",
           section: "Labs",handler: () => {
               window.location.href = "/ece5160-labreports/labs/lab1.html";
+            },},{id: "labs-lab-10",
+          title: 'Lab 10',
+          description: "Grid Localization using Bayes Filter (Simulation)",
+          section: "Labs",handler: () => {
+              window.location.href = "/ece5160-labreports/labs/lab10.html";
             },},{id: "labs-lab-2",
           title: 'Lab 2',
           description: "Inertial Measurement Unit (IMU)",
